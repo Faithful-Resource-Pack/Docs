57c91f9425f652752a35dd64d1d68208a6eaa77e
TheRolf <yann.lv@live.fr>
2022-07-21 13:28:10 +0200

    good header

--- src/routes/layout.svelte
+++ src/routes/layout.svelte
@@ -2,6 +2,7 @@
 
 <script lang="ts">
 	import { browser } from '$app/env';
+	import { page } from "$app/stores";
 	import Header from '../components/header.svelte';
 	import '../css/app.scss';
 	import { theme } from "../lib/stores";
@@ -9,11 +10,22 @@
 	// mdsvex title prop
 	export let title;
 
+	$: domain = $page.url.hostname;
+	$: full_title = `Faithful Pack Docs - ${title}`;
 	$: year = new Date().getFullYear().toString();
+	$: url = $page.url.href;
 </script>
 
 <svelte:head>
-	<title>Faithful Pack Docs - {title}</title>
+	<title>{full_title}</title>
+
+	<meta property="og:title" content={full_title}>
+	<meta property="og:url" content={url}>
+	
+	<meta name="twitter:title" content={full_title}>
+	<meta property="twitter:domain" content={domain}>
+	<meta property="twitter:url" content={url}>
+	
 	{#if $theme === 'dark' || ($theme === 'auto' && browser && window.matchMedia('(prefers-color-scheme: dark)').matches)}
 		<style lang="scss">
 			@use "../css/dark.scss";
