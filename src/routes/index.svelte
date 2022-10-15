26828ee2db9318f44d6d70ad132c5e4c14551216
Robert <contact.e3nk6@aleeas.com>
2022-10-16 00:24:03 +0200

    fix #12

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -6,6 +6,26 @@
 	const categories: string[] = getContext('categories');
 
 	let title = "Home";
+
+	let isNew = (date: string) => {
+    if(date === undefined) return false;
+
+    let delayedDate = new Date();
+    delayedDate.setMonth(delayedDate.getMonth() - 1);
+
+    let postDate = new Date();
+    let args = date.split('/').map(el => Number.parseInt(el, 10));
+    postDate.setDate(args[0]);
+    postDate.setMonth(args[1] - 1);
+    postDate.setFullYear(args[2]);
+
+    if (isNaN(postDate.getTime())) return false;
+
+    /**
+     * If the post date is greater than today's date - 1 month, show the span
+     */
+    return postDate.getTime() > delayedDate.getTime()
+  }
 </script>
 
 <Layout title="Home">
@@ -31,6 +51,8 @@
 				<li>
 					<a href={post.path}>
 						{post.meta.title}
+						{#if isNew(post.meta.date) }<span class="new-badge keepTag" style="margin-right: 5px;">NEW</span>{/if}
+						{#if 'deprecated' in post.meta && post.meta.deprecated}<span class="deprecated-badge">DEPRECATED</span>{/if}
 					</a>
 				</li>
 			{/each}
